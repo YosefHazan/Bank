ed11e34de20cd9f4af89b286ce104f8677a5e179
‘YosefHazan‘ <yosef771@gmail.com>
2019-07-07 21:23:02 +0300

    yosef

---
+++ src/app/bankcom/bankcom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bankcom',
+  templateUrl: './bankcom.component.html',
+  styleUrls: ['./bankcom.component.css']
+})
+export class BankcomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
