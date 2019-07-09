9a0134fe0b0d89066093d627bb9782410d3f5ad3
‘YosefHazan‘ <yosef771@gmail.com>
2019-07-10 01:44:32 +0300

    addmodelonly

---
+++ src/app/model/bank-acount.spec.ts
@@ -0,0 +1,7 @@
+import { BankAcount } from './bank-acount';
+
+describe('BankAcount', () => {
+  it('should create an instance', () => {
+    expect(new BankAcount()).toBeTruthy();
+  });
+});
---
+++ src/app/model/bank-action.spec.ts
@@ -0,0 +1,7 @@
+import { BankAction } from './bank-action';
+
+describe('BankAction', () => {
+  it('should create an instance', () => {
+    expect(new BankAction()).toBeTruthy();
+  });
+});
